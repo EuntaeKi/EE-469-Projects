fc24d91ee08a240a86975518fe1cf4491d3c006f
Ian Davidson <35235676+iansd-1733208@users.noreply.github.com>
2019-10-11 09:49:24 -0700

    Renamed dflipflop.sv

--- Lab1/Launch_ModelSim.bat
+++ Lab1/Launch_ModelSim.bat
@@ -1,1 +1,6 @@
-E:\IntelFPGAQuartus\modelsim_ase\win32aloem\modelsim.exe
+if exist e:\ ( 
+	E:\IntelFPGAQuartus\modelsim_ase\win32aloem\modelsim.exe 
+) else (
+	C:\intelFPGA_lite\17.0\modelsim_ase\win32aloem\modelsim.exe 	
+)
+
