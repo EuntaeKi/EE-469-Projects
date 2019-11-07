b130cfbf4c3dace4327464b2cbc446babb09e334
Euntae <rldmsxo@gmail.com>
2019-11-06 21:19:42 -0800

    ControlSelect has been modified with comments

--- Lab3/ControlSelect.sv
+++ Lab3/ControlSelect.sv
@@ -1,16 +1,36 @@
-module CPUcontrol (CondAddr19, BrAddr26, UncondBr, BrTaken, clk, reset, opCode);
-	input logic [18:0] CondAddr19;
-	input logic [25:0] BrAddr26;
+module CPUcontrol (clk, reset, UncondBr, BrTaken, Instruction);
+	// Input Logic
 	input logic clk, reset, UncondBr, BrTaken;
-	logic [63:0] condAddr, condAddrEx, brAddrEx, shiftedCond, BrAddr, PC, updatedPC;
-	output logic [31:0] opCode;
 	
-	MUX64_2_1 condMUX (.a(condAddrEx), .b(brAddrEx), .en(UncondBr), .out(condAddr));
-	MUX2_1 brMUX (.a(updatedPC + 3'b100), .b(brAddr), .en(BrTaken), .out(updatedPC));
-	signExtend cond (.in(CondAddr19), .out(condAddrEx));
-	signExtend brCond (.in(BrAddr26), .out(brAddrEx));
-	shifter shift (.value(condAddr), .direction(1'b0), .distance(5'b00010), .result(shiftedCond));
-	fullAdder_64 add (.a(PC), .b(shiftedCond), .out(BrAddr));
-	programCounter pc (.in(updatedPC), .clk, .reset, .out(PC));
-	instructmem instmem (.address(PC), .instruction(opCode), .clk);	
+	// Output Logic
+	output logic [31:0] Instruction;
+	
+	// Intermediate Logic
+	logic [63:0] condAddr, brAdder, muxedAddr, shiftedAddr, branchedAddr, currentPC, updatedPC;
+	
+	// Sign Extend the address inputs
+	// Instruction[23:5] is CondAddr19
+	// Instruction[25:0] is BrAddr26
+	signExtend signExtendCondAddr (.in(Instruction[25:0]), .out(condAddr));
+	signExtend signExtendBrAddr (.in(Instruction[23:5]), .out(brAddr));
+
+	// MUX whether or not it's an unconditional branch
+	MUX64_2_1 condMUX (.a(condAddr), .b(brAddr), .en(UncondBr), .out(muxedAddr));
+	
+	// Shift the muxedAddr by 2 bits to multiply it by 4
+	shifter shift (.value(condAddr), .direction(1'b0), .distance(5'b00010), .result(shiftedAddr));
+
+	// Add the shiftedAddr with currentPC
+	fullAdder_64 add (.a(currentPC), .b(shiftedCond), .out(branchedAddr));
+
+	// Determine if branch instruction was given or not
+	// The result goes into PC regardless to update the PC
+	MUX2_1 brMUX (.a(currentPC + 3'b100), .b(branchedAddr), .en(BrTaken), .out(updatedPC));
+
+	// Register that hold the ProgramCounter
+	// Current PC gets fed into IM (Instruction Memory)
+	programCounter pc (.in(updatedPC), .clk, .reset, .out(currentPC));
+
+	// Feed the address and will Fetches the Instruction into the top-level module
+	instructmem instmem (.address(currentPC), .instruction(Instruction), .clk);	
 endmodule 
