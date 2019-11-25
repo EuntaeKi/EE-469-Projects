dd1d00a57015a3d5ab4027da8d77298844963c8b
Euntae <rldmsxo@gmail.com>
2019-11-25 10:57:05 -0800

    Register inputs are fixed (Hopefully. again).

--- Lab4/DecodeRegister.sv
+++ Lab4/DecodeRegister.sv
@@ -1,34 +1,53 @@
 `timescale 1ns/10ps
 
-module DecodeRegister (clk, reset, DecDa, DecDb, DecALUOp, DecALUSrc, DecMem2Reg, DecMemWrite, DecMemRead, DecFlagWrite, DecRegWrite, DecMemAddr9Ext, DecImm12Ext, 
-								ExDa, ExDb, ExALUOp, ExALUSrc, ExMem2Reg, ExMemWrite, ExMemRead, ExFlagWrite, ExRegWrite, ExMemAddr9Ext, ExImm12Ext);
+module DecodeRegister (clk, reset, 
+                        DecPC, DecALUOp, DecALUSrc, DecMem2Reg, DecBrTaken, 
+                        DecReg2Write, DecRegWrite, DecMemWrite, DecMemRead, DecRn,
+                        DecRm, DecRd, DecDa, DecDb, DecImm12Ext, DecImm9Ext, DecImmBranch,
+
+                        ExPC, ExALUOp, ExALUSrc, ExMem2Reg, ExBrTaken, 
+                        ExReg2Write, ExRegWrite, ExMemWrite, ExMemRead, ExRn, 
+                        ExRm, ExRd, ExDa, ExDb, ExImm12Ext, ExImm9Ext, ExImmBranch)
 
     // Input Logic
     input  logic        clk, reset;
-    input  logic [63:0] DecDa, DecDb, DecMemAddr9Ext, DecImm12Ext;
+    input  logic [63:0] DecPC, DecDa, DecDb, DecImm9Ext, DecImm12Ext, DecImmBranch;
+    input  logic [4:0]  DecRn, DecRm, DecRd;
     input  logic [2:0]  DecALUOp;
-    input  logic [1:0]  DecMem2Reg, DecALUSrc;
-    input  logic        DecMemWrite, DecMemRead, DecFlagWrite, DecRegWrite;
+    input  logic [1:0]  DecMem2Reg, DecALUSrc, DecBrTaken;
+    input  logic        DecMemWrite, DecMemRead, DecFlagWrite, DecRegWrite, DecReg2Write;
     
     // Output Logic
-    output logic [63:0] ExDa, ExDb, ExMemAddr9Ext, ExImm12Ext;
+    output logic [63:0] ExPC, ExDa, ExDb, ExImm9Ext, ExImm12Ext, ExImmBranch;
+    output logic [4:0]  ExRn, ExRm, ExRd;
     output logic [2:0]  ExALUOp;
-    output logic [1:0]  ExMem2Reg, ExALUSrc;
-    output logic        ExMemWrite, ExMemRead, ExFlagWrite, ExRegWrite;
+    output logic [1:0]  ExMem2Reg, ExALUSrc, ExBrTaken;
+    output logic        ExMemWrite, ExMemRead, ExFlagWrite, ExRegWrite, ExReg2Write;
 
     // Register Instantiation
-    register64 ExDaReg (.reset, .clk, .write(1'b1), .in(DecDa), .out(ExDa));
-    register64 ExDbReg (.reset, .clk, .write(1'b1), .in(DecDb), .out(ExDb));
-    register64 ExMemAddrReg (.reset, .clk, .write(1'b1), .in(DecMemAddr9Ext), .out(ExMemAddr9Ext));
-    register64 ExImmReg (.reset, .clk, .write(1'b1), .in(DecImm12Ext), .out(ExImm12Ext));
+    register64 PCReg (.reset, .clk, .write(1'b1), .in(DecPC), .out(ExPC));
+    register64 DaReg (.reset, .clk, .write(1'b1), .in(DecDa), .out(ExDa));
+    register64 DbReg (.reset, .clk, .write(1'b1), .in(DecDb), .out(ExDb));
+    register64 Imm9Reg (.reset, .clk, .write(1'b1), .in(DecImm9Ext), .out(ExImm9Ext));
+    register64 Imm12Reg (.reset, .clk, .write(1'b1), .in(DecImm12Ext), .out(ExImm12Ext));
+    register64 ImmBranchReg (.reset, .clk, .write(1'b1), .in(DecImmBranch), .out(ExImmBranch));
 
+    // Register Address Registers
+    registerN #(.N(5)) RnReg (.reset, .clk, .in(DecRn), .out(ExRn));
+    registerN #(.N(5)) RmReg (.reset, .clk, .in(DecRm), .out(ExRm));
+    registerN #(.N(5)) RdReg (.reset, .clk, .in(DecRd), .out(ExRd));
+
+    // Control Logic Registers (n-bits)
     registerN #(.N(3)) ALUOpReg (.reset, .clk, .in(DecALUOp), .out(ExALUOp));
     registerN #(.N(2)) ALUSrcReg (.reset, .clk, .in(DecALUSrc), .out(ExALUSrc));
     registerN #(.N(2)) Mem2RegReg (.reset, .clk, .in(DecMem2Reg), .out(ExMem2Reg));
+    registerN #(.N(2)) BrTakenReg (.reset, .clk, .in(DecBrTaken), .out(ExBrTaken));
 
+    // Control Logic Registers (1-bit)
     D_FF MemWriteReg (.q(ExMemWrite), .d(DecMemWrite), .reset, .clk);
     D_FF MemReadReg (.q(ExMemRead), .d(DecMemRead), .reset, .clk);
     D_FF FlagWriteReg (.q(ExFlagWrite), .d(DecFlagWrite), .reset, .clk);
     D_FF RegWriteReg (.q(ExRegWrite), .d(DecRegWrite), .reset, .clk);
+    D_FF Reg2WriteReg (.q(ExReg2Write), .d(DecReg2Write), .reset, .clk);
     
 endmodule
--- Lab4/ExecRegister.sv
+++ Lab4/ExecRegister.sv
@@ -1,26 +1,39 @@
 `timescale 1ns/10ps
 
-module ExecRegister (clk, reset, ExDb, ExALUOutput, ExMem2Reg, ExMemWrite, ExMemRead, ExRegWrite,
-                     MemDb, MemALUOutput, MemMem2Reg, MemMemWrite, MemMemRead, MemRegWrite);
+module ExecRegister (clk, reset, 
+                    ExMem2Reg, ExBrTaken, ExRegWrite, ExMemWrite, 
+                    ExMemRead, ExRn, ExRm, ExRd, ExDb, ExBrPC, ExALUOut,
+                    
+                    MemMem2Reg, MemBrTaken, MemRegWrite, MemMemWrite, 
+                    MemMemRead, MemRn, MemRm, MemRd, MemDb, MemBrPC, MemALUOut);
 
     // Input Logic
     input  logic        clk, reset;
-    input  logic [63:0] ExDb, ExALUOutput;
-    input  logic [1:0]  ExMem2Reg;
+    input  logic [63:0] ExDb, ExALUOut, ExBrPC;
+    input  logic [4:0]  ExRn, ExRm, ExRd;
+    input  logic [1:0]  ExMem2Reg, ExBrTaken;
     input  logic        ExMemWrite, ExMemRead, ExRegWrite;
 
-    // Output Logic (ExOutput)
-    output logic [63:0] MemDb, MemALUOutput;
-    output logic [1:0]  MemMem2Reg;
+    // Output Logic
+    output logic [63:0] MemDb, MemALUOut, MemBrPC;
+    output logic [4:0]  MemRn, MemRm, MemRd;
+    output logic [1:0]  MemMem2Reg, MemBrTaken;
     output logic        MemMemWrite, MemMemRead, MemRegWrite;
 
     // Register Instantiation
-    register64 ALUOutputReg (.reset, .clk, .write(1'b1), .in(ExALUOutput), .out(MemALUOutput));
+    register64 ALUOutReg (.reset, .clk, .write(1'b1), .in(ExALUOut), .out(MemALUOut));
+    register64 BrPCReg (.reset, .clk, .write(1'b1), .in(ExBrPC), .out(MemBrPC));
     register64 DbReg (.reset, .clk, .write(1'b1), .in(ExDb), .out(MemDb));
 
+    // Register Address Register Instantiation
+    registerN #(.N(5)) RnReg (.reset, .clk, .in(ExRn), .out(MemRn));
+    registerN #(.N(5)) RmReg (.reset, .clk, .in(ExRm), .out(MemRm));
+    registerN #(.N(5)) RdReg (.reset, .clk, .in(ExRd), .out(MemRd));
     registerN #(.N(2)) Mem2RegReg (.reset, .clk, .in(ExMem2Reg), .out(MemMem2Reg));
 
+    // Control Bit
     D_FF MemWriteReg (.q(MemMemWrite), .d(ExMemWrite), .reset, .clk);
     D_FF MemReadReg (.q(MemMemRead), .d(ExMemRead), .reset, .clk);
+    D_FF RegWriteReg (.q(MemRegWrite), .d(ExRegWrite), .reset, .clk);
 
 endmodule
